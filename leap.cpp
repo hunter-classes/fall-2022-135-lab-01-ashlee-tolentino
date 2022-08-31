4cd28aecac9897a380b6b494738d5417be0aec6b
Ashlee Tolentino <ashleetolentino@Ashlees-MacBook-Air.local>
2022-08-31 12:13:04 -0400

    finished the code for Lab1C

---
+++ leap.cpp
@@ -0,0 +1,34 @@
+/*
+Author: Ashlee Tolentino
+Course: CSCI-135
+Instructor: Michael Zamansky
+Assignment: Lab1C
+
+This program asks the user to input an integer representing a year number. If the input year is a leap year, it should print "Leap year", otherwise, it prints "Common year".
+*/
+
+#include <iostream>
+
+int main() 
+{
+	std::cout << "Enter year: " << std::endl;
+	int year;
+	std::cin >> year;
+
+	if (year % 4 != 0)
+	{
+		std::cout << "Common year" << std::endl;
+	}
+	else if (year % 100 != 0)
+	{
+		std::cout << "Leap year" << std::endl;
+	}
+	else if (year % 400 != 0)
+	{
+		std::cout << "Common year" << std::endl;
+	}
+	else
+	{
+		std::cout << "Leap year" << std::endl;
+	}
+}
